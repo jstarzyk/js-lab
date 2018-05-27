ffdd7ea0856a430c2fbcb1cc3904f5521b063f99
jakub <jstarzyk98@gmail.com>
2018-05-27 19:20:19 +0200

    Database seeding - almost complete

--- deanery/seed.js
+++ deanery/seed.js
@@ -4,30 +4,47 @@ const Subject = schemas.Subject;
 const Assignment = schemas.Assignment;
 const Student = schemas.Student;
 const Teacher = schemas.Teacher;
-// const ObjectId = require('mongoose').Schema.ObjectId;
 
-// let teacherUser = new User({
-//     username: "spolak",
-//     password: "alamakota",
-//     role: "teacher"
-// });
-//
-// let teacher = new Teacher({
-//     firstName: "Stanislaw",
-//     lastName: "Polak",
-//     user: teacherUser._id
-// });
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/deanery')
+    .then(db => console.log('connected'))
+    .catch(err => console.error(err.message));
+
+seed();
 
 function seed() {
     let subjects = [];
     let students = [];
 
+    let personPromises = [
+        addPersonWithUser("jkowalski", "abc123jk", "Jan", "Kowalski", "student"),
+        addPersonWithUser("amalinowski", "abc123am", "Adam", "Malinowski", "student")
+    ];
 
-
-    let teacherPromise = addPersonWithUser("spolak", "alamakota", "Stanislaw", "Polak", "teacher");
-    teacherPromise.then((t))
-    addSubjectWithAssignments("JavaScript", 7, )
+    Promise.all(personPromises)
+        .then(results => results.forEach(student => students.push(student)))
+        .then(() => Promise.all([
+            addSubjectWithAssignments("JavaScript", 7, students),
+            addSubjectWithAssignments("Erlang", 7, students),
+        ]))
+        .then(results => results.forEach(subject => subjects.push(subject)))
+        .then(() => {
+            let marksPromises = [];
+            students.forEach(student => {
+                // console.log(student);
+                subjects.forEach(subject => marksPromises.push(addRandomMarksToStudent(student._id, subject._id)));
+                // subjects.forEach(subject => {
+                //     console.log(subject);
+                //     marksPromises.push(addRandomMarksToStudent(student, subject))
+                // });
+            });
+            return Promise.all(marksPromises);
+        })
+        .then(() => addPersonWithUser("spolak", "abc123sp", "Stanisław", "Polak", "teacher"))
+        .then(teacher => addSubjectsToTeacher(teacher._id, subjects))
+        .then(() => mongoose.disconnect())
+        .catch(err => console.error(err));
 }
 
 function addSubjectWithAssignments(name, nAssignments, students) {
@@ -40,39 +57,15 @@ function addSubjectWithAssignments(name, nAssignments, students) {
         assignments.push(assignment);
     }
 
-    Assignment.insertMany(assignments).then((assignments) => {
+    return Assignment.insertMany(assignments).then((assignments) => {
         let subject = new Subject({
             name: name,
             assignments: assignments,
             students: students
         });
+
         return subject.save();
     });
-
-    // for (let i = 1; i <= nAssignments; i++) {
-    //     let assignment = new Assignment({
-    //         name: "lab" + i
-    //     });
-    //
-    //     assignments.push(assignment);
-
-    //     assignment.save()
-    //         .then((assignment) => {
-    //             if (i === nAssignments) {
-    //                 let subject = new Subject({
-    //                     name: name,
-    //                     assignments: assignments,
-    //                     students: students
-    //                 });
-    //
-    //                 // subject.save((err) => {
-    //                 //     if (err) console.error("save failed");
-    //                 // });
-    //
-    //                 return subject.save();
-    //             }
-    //         });
-    // }
 }
 
 function addPersonWithUser(username, password, firstName, lastName, role) {
@@ -82,9 +75,7 @@ function addPersonWithUser(username, password, firstName, lastName, role) {
         role: role
     });
 
-    return user.save((err) => {
-        if (err) console.error("save failed");
-    }).then((user) => {
+    return user.save().then((user) => {
         let person;
 
         if (role === "student") {
@@ -106,56 +97,40 @@ function addPersonWithUser(username, password, firstName, lastName, role) {
 }
 
 function addSubjectsToTeacher(id, subjects) {
-    Teacher.findById(id).exec((err, teacher) => {
-        if (err) console.error("findById failed");
-
+    return Teacher.findById(id).then(teacher => {
         teacher.subjects = subjects;
+
+        return teacher.save();
     });
 }
 
-// function addStudentWithUser(username, password, firstName, lastName) {
-//     let studentUser = new User({
-//         username: username,
-//         password: password,
-//         role: "student"
-//     });
-//
-//     studentUser.save((err) => {
-//         if (err) console.error("save failed");
-//
-//         let student = new Student({
-//             user: studentUser._id,
-//             firstName: firstName,
-//             lastName: lastName,
-//         });
-//
-//         student.save((err) => {
-//             if (err) console.error("save failed");
-//         })
-//     });
-//
-//     return studentUser._id;
-// }
+function addRandomMarksToStudent(studentId, subjectId) {
+    let studentPromise = Student.findById(studentId);
+    let subjectPromise = Subject.findById(subjectId);
 
-function addRandomMarksToStudent(id, nMarks, subject) {
-    Student.findById(id).exec((err, student) => {
-        if (err) console.error("findById failed");
-
-        Subject.findById(subject._id).exec((err, subject) => {
-            let marks = [];
-            let assignments = subject.assignments;
-
-            for (let i = 0; i < nMarks; i++) {
-                marks.push({
-                    subject: subject,
-                    assignment: assignments[i],
-                    value: Math.floor(Math.random() * ((5 - 2) + 1) + 2)
-                })
+    Promise.all([studentPromise, subjectPromise]).then(([student, subject]) => {
+        subject.assignments.map(assignment => {
+            return {
+                subject: subject._id,
+                assignment: assignment._id,
+                value: Math.floor(Math.random() * ((5 - 2) + 1) + 2)
             }
+        }).forEach(mark => student.marks.push(mark));
 
-            student.marks = marks;
-        });
+        return student.save();
     });
 }
 
-
+// TODO: Check if possible to pass object instead of id
+// function addRandomMarksToStudent(student, subject) {
+//     subject.assignments.map(a => {
+//         return {
+//             subject: subject._id,
+//             assignment: a._id,
+//             value: Math.floor(Math.random() * ((5 - 2) + 1) + 2)
+//         }
+//     }).forEach(m => student.marks.push(m));
+//
+//     return student.save();
+// }
+//
